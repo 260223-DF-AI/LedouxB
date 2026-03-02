1238d7a6490655f2647ecf42b83a616248c7b0c6
Benjamin Ledoux <bledoux2002@gmail.com>
2026-03-02 10:54:42 -0600

    fixed simple and merge sorting in test.py. was WAY off with simple. also added random array of 100 ints to sort for more robust testing

--- test.py
+++ test.py
@@ -1,43 +1,55 @@
 # Ben Ledoux, 2/25/2026 9:52 AM CT
 # Test python file in terminal
 
+import time
+from random import randint
+
 def main():
 	print("Hello, World!")
-	numList = [5, 7, 4, 2, 8, 9, 5, 10, 1, 144, 32, 2934, 453, 3]
-	print(f"Current list: {numList}")
+	# numList = [5, 7, 4, 2, 8, 9, 5, 10, 1, 144, 32, 2934, 453, 3]
+	numList = [randint(1, 100) for _ in range(100)]
+	# print(f"Starting list: {numList}")
+	
+	start = time.time()
 	sortedList = simpleSort(numList)
-	print(f"Sorted list, O(n^2): {sortedList}")
+	end = time.time()
+	print(f"Sorted list, O(n^2), runtime {(end - start) * 1000:.2f}ms")
+	print(sortedList)
+	
+	start = time.time()
 	binarySortedList = binarySort(numList)
-	print(f"Sorted list, O(log(n)): {binarySortedList}")
+	end = time.time()
+	print(f"Sorted list, O(log(n)), runtime {(end - start) * 1000:.2f}ms")
+	print(binarySortedList)
+	
 	target = int(input("Enter number to search for: "))
 	index = binarySearch(target, binarySortedList, 0)
 	if index == -1:
 		print("Target number not found in sorted list...")
 	else:
-		print(f"Target number found at index {index}: {sortedList[index]}")
+		print(f"Target number found at index {index}: {binarySortedList[index]}")
 
 def simpleSort(list):
-	for i, num in enumerate(list):
-		for j in range(i + 1, len(list)):
-			if (num > list[j]):
-				temp = list[j]
-				list[j] = num
-				list[i] = temp
+	n = len(list)
+	for i in range(n):
+		for j in range(n - i - 1):
+			if (list[j] > list[j + 1]):
+				list[j], list[j + 1] = list[j + 1], list[j]
 	return list
 
-def binarySearch(num, list, i):
+def binarySearch(target, list: list, i: int) -> int:
 	if (len(list) < 1):
 		return -1
 	n = len(list) // 2
-	if (num == list[n]):
+	if (target == list[n]):
 		return i + n
-	elif (num < list[n]):
-		return binarySearch(num, list[:n], 0)
-	elif (num > list[n]):
-		return binarySearch(num, list[n+1:], i+n+1)
+	elif (target < list[n]):
+		return binarySearch(target, list[:n], i)
+	elif (target > list[n]):
+		return binarySearch(target, list[n+1:], i+n+1)
 	return -1
 
-def binarySort(list):
+def binarySort(list: list) -> list:
 	n = len(list)
 	if (n < 2):
 		return list
