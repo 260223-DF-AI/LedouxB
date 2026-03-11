0b85c111f40dc54a3a762ac9e7ef66d2a36b33db
Benjamin Ledoux <bledoux2002@gmail.com>
2026-03-11 11:19:56 -0500

    sqlalchemy demo progress

--- W3/associates/training.py
+++ W3/associates/training.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.types import Integer, String, Date
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 # connection requirements (from CLI)
@@ -46,7 +47,7 @@ df.to_sql(
 
 print("Wrote to db")
 
-from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -54,20 +55,25 @@ class Base(DeclarativeBase):
 class Cohort(Base):
     __tablename__ = "cohorts"
     
-    id: Mapped[int]
-    start_date: Mapped[Date]
-    curriculum: Mapped[str]
-    trainees: Mapped[list["Trainee"]]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    start_date: Mapped[datetime] = mapped_column(default=datetime.today())
+    curriculum: Mapped[str] = mapped_column(nullable=False)
+    trainees: Mapped[list["Trainee"]] = relationship(back_populates="cohort")
 
 
 
 class Trainee(Base):
     __tablename__ = "trainees"
 
-    id: Mapped[int]
-    first_name: Mapped[str]
-    last_name: Mapped[str]
-    email: Mapped[str]
-    hire_date: Mapped[Date]
-    department: Mapped[str]
-    cohort: Mapped["Cohort"]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    hire_date: Mapped[datetime] = mapped_column(default=datetime.today())
+    department: Mapped[str] = mapped_column(nullable=False)
+
+    cohort_id: Mapped[int] = mapped_column(ForeignKey("cohorts.id"))
+    cohort: Mapped["Cohort"] = relationship(back_populates="trainees")
+
+
+Base.metadata.create_all(engine)
